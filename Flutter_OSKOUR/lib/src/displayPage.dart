8ad6f9a9d22ed14fc5809c470732a62a82f755aa
Pablo de sousa <pablo@desousa.com>
2020-11-08 00:00:09 +0100

    [FRONT] Adding all fluter code with analysis verifier

---
+++ Flutter_OSKOUR/lib/src/displayPage.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:OSKOUR/api.dart';
+import 'package:OSKOUR/src/missionPage.dart';
+import 'package:OSKOUR/src/displayTrip.dart';
+import 'package:OSKOUR/src/camera.dart';
+import 'package:OSKOUR/src/Factory.dart';
+
+class DisplayPage extends StatefulWidget {
+  const DisplayPage({Key key, this.title, this.token}) : super(key: key);
+
+  final String title;
+  final String token;
+
+  @override
+
+  _DisplayPageState createState() => _DisplayPageState();
+}
+
+final Policies policies = Policies(
+  fetch: FetchPolicy.networkOnly,
+);
+
+class _DisplayPageState extends State<DisplayPage> {
+
+  Widget _displayPlane() {
+    return Query(
+        options: QueryOptions(
+          documentNode: gql(getPlane),
+          // this is the query string you just created
+          pollInterval: 50,
+        ),
+        builder: (QueryResult result,
+            {VoidCallback refetch, FetchMore fetchMore}) {
+          if (result.hasException) {
+            return Text(result.exception.toString());
+          }
+
+          if (result.loading) {
+            return const Text('Loading');
+          }
+          final LaunchList launches = LaunchList.fromJson(result.data['launches']['launches'] as List<dynamic>);
+          return ListView.builder(
+              itemCount: launches.launchList.length,
+              shrinkWrap: true,
+              itemBuilder: (BuildContext context,int index) {
+                final LaunchItem launch = launches.launchList[index];
+                return GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context, MaterialPageRoute<void>(builder: (BuildContext context) => MissionPage(id: launch.missionID, token: widget.token)));
+                    },
+                  child: Card(
+                    child: ListTile(
+                      title: Text(launch.mission.name),
+                    )
+                  )
+                );
+              });
+        });
+  }
+
+  @override
+
+  Widget build(BuildContext context) {
+    final ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
+      GraphQLClient(
+        link: HttpLink(uri: 'https://flutter-spacex.herokuapp.com/v1/graphql', headers: <String, String> {
+          'Authorization': 'Bearer ' + widget.token,
+        }),
+        cache: InMemoryCache(),
+        defaultPolicies: DefaultPolicies(
+          watchQuery: policies,
+          query: policies,
+          mutate: policies,
+        ),
+      ),
+    );
+    return GraphQLProvider(
+      child: Scaffold(
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.white,
+            selectedItemColor: Colors.white,
+            unselectedItemColor: Colors.white.withOpacity(.60),
+            selectedFontSize: 14,
+            unselectedFontSize: 14,
+            onTap: (int value) {
+              if (value == 1) {
+                Navigator.push(
+                    context, MaterialPageRoute<void>(builder: (BuildContext context) => DisplayTrip(token: widget.token)));
+              }
+              if(value == 0) {
+                Navigator.push(
+                    context, MaterialPageRoute<void>(builder: (BuildContext context) => DisplayPage(token: widget.token)));
+              }
+              if(value == 2) {
+                Navigator.push(
+                    context, MaterialPageRoute<void>(builder: (BuildContext context) => CameraPage(token: widget.token)));
+              }
+            },
+            items: const <BottomNavigationBarItem> [
+              BottomNavigationBarItem(
+                title: Text('Home',
+                    style: TextStyle(
+                      color: Colors.black,
+                    )),
+                icon: Icon(
+                  Icons.home,
+                  color: Colors.black,
+                ),
+              ),
+              BottomNavigationBarItem(
+                title: Text('Your trip',
+                    style: TextStyle(
+                      color: Colors.black,
+                    )),
+                icon: Icon(
+                  Icons.airplanemode_active,
+                  color: Colors.black,
+                ),
+              ),
+              BottomNavigationBarItem(
+                title: Text('Profile',
+                    style: TextStyle(
+                      color: Colors.black,
+                    )),
+                icon: Icon(
+                  Icons.person,
+                  color: Colors.black,
+                ),
+              )
+            ],
+          ),
+          body: _displayPlane()
+      ),
+      client: client,
+    );
+  }
+}
---
+++ Flutter_OSKOUR/lib/src/loginPage.dart
@@ -0,0 +1,240 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:OSKOUR/api.dart';
+import 'package:OSKOUR/src/signup.dart';
+import 'package:OSKOUR/src/displayPage.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+final Policies policies = Policies(
+  fetch: FetchPolicy.networkOnly,
+);
+
+ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
+  GraphQLClient(
+    link: HttpLink(uri: 'https://flutter-spacex.herokuapp.com/v1/graphql'),
+    cache: InMemoryCache(),
+    defaultPolicies: DefaultPolicies(
+      watchQuery: policies,
+      query: policies,
+      mutate: policies,
+    ),
+  ),
+);
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController username = TextEditingController();
+  final TextEditingController password = TextEditingController();
+
+  Widget _backButton() {
+    return InkWell(
+      onTap: () {
+        Navigator.pop(context);
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Row(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.only(left: 0, top: 10, bottom: 10),
+              child: const Icon(Icons.keyboard_arrow_left, color: Colors.white),
+            ),
+            const Text('Back',
+                style: TextStyle(
+                    fontSize: 12,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white))
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _passwordField(String title, {bool isPassword = true}) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(
+            height: 10,
+          ),
+          TextField(
+              obscureText: isPassword,
+              controller: password,
+              decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  fillColor: Color(0xfff3f3f4),
+                  filled: true))
+        ],
+      ),
+    );
+  }
+
+  Widget _usernameField(String title, {bool isPassword = false}) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(
+            height: 10,
+          ),
+          TextField(
+              obscureText: isPassword,
+              controller: username,
+              decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  fillColor: Color(0xfff3f3f4),
+                  filled: true))
+        ],
+      ),
+    );
+  }
+
+  Widget _submitButton() {
+    return Mutation(
+        options: MutationOptions(
+          documentNode: gql(getUserLogin),
+          update: (Cache cache, QueryResult result) {
+          },
+          // or do something with the result.data on completion
+          onCompleted: (dynamic resultData) {
+            print(resultData);
+            Navigator.push(
+                context, MaterialPageRoute<void>(builder: (BuildContext context) => DisplayPage(token: resultData['Login']['token'] as String)));
+          },
+        ),
+        builder: (RunMutation runMutation, QueryResult result) {
+          print(result.exception);
+          return GestureDetector(
+            onTap: () {
+              runMutation(<String, dynamic> {'username': username.text, 'password': password.text});
+            },
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              padding: const EdgeInsets.symmetric(vertical: 15),
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  color: Colors.white),
+              child: const Text(
+                'Login',
+                style: TextStyle(fontSize: 20, color: Colors.black),
+              ),
+            ),
+          );
+        });
+  }
+
+  Widget _createAccountLabel() {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context, MaterialPageRoute<void>(builder: (BuildContext context) => const SignUpPage()));
+      },
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 20),
+        padding: const EdgeInsets.all(15),
+        alignment: Alignment.bottomCenter,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'Don\'t have an account ?',
+              style: TextStyle(
+                fontSize: 13,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              'Register',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w600),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _title() {
+    return RichText(
+      textAlign: TextAlign.center,
+      text: TextSpan(
+          text: 'OSKOUR',
+          style: GoogleFonts.portLligatSans(
+            textStyle: Theme.of(context).textTheme.headline4,
+            fontSize: 30,
+            fontWeight: FontWeight.w700,
+            color: Colors.white,
+          )),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final double height = MediaQuery.of(context).size.height;
+    return GraphQLProvider (
+      child: Scaffold(
+          body: Container(
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(5)),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                    color: Colors.grey.shade200,
+                    offset: const Offset(2, 4),
+                    blurRadius: 5,
+                    spreadRadius: 2)
+              ],
+              image: const DecorationImage(
+                image: AssetImage('assets/img/Background.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            height: height,
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        SizedBox(height: height * .2),
+                        _title(),
+                        const SizedBox(height: 50),
+                        _usernameField('username'),
+                        const SizedBox(height: 50),
+                        _passwordField('password'),
+                        const SizedBox(height: 20),
+                        _submitButton(),
+                        SizedBox(height: height * .055),
+                        _createAccountLabel(),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(top: 40, left: 0, child: _backButton()),
+              ],
+            ),
+          )),
+      client: client,
+    );
+  }
+}
